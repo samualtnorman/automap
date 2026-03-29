884a78e2ed0bfa14fcbd2384707e42e6a928be52
Samual Norman <me@samual.uk>
2026-03-30 00:27:29 +0100

    don't require `TARGET` environment variable in `rolldown.config.js`

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,9 +1,8 @@
 #!node_modules/.bin/rolldown --config
 import { rolldownConfig } from "@samual/rolldown-config"
 import Path from "path"
-import { env } from "./scripts/common.js"
 
-export default rolldownConfig(env("TARGET") != "jsr" ? undefined : {
+export default rolldownConfig(process.env.TARGET != "jsr" ? undefined : {
 	rolldownOptions: { output: { banner: chunk => `// @ts-self-types="./${Path.basename(chunk.name)}.d.ts"` } },
 	terserOptions: { format: { comments: /@ts-self-types/ } }
 })
